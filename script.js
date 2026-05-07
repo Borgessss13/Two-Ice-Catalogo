4a98b5dc2652537ef9ceb0c8eb75fcbbc8480b0a
Borgessss13 <100052310+Borgessss13@users.noreply.github.com>
2026-05-07 18:19:58 +0000

    Update Portugal Principal 2026 photo filename

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const baseDeDados = {
         { id: "slb", nome: "Benfica", foto: "fotos/benfica.png" }
     ],
     "portugal": [
-        { id: "pt_home", nome: "Portugal Principal 2026", foto: "ptprin2627.jpg" },
+        { id: "pt_home", nome: "Portugal Principal 2026", foto: "ptprin2526.jpg" },
         { id: "pt_away", nome: "Portugal Away 2026", foto: "https://yupoo.com" },
         { id: "pt_treino", nome: "Portugal Treino 2026", foto: "https://yupoo.com" },
         { id: "pt_retro", nome: "Portugal Edição Retro", foto: "https://yupoo.com" }
