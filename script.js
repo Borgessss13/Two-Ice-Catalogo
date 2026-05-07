7af8c5eef9a3af05fcdb6ecc14191755dcb2e416
Borgessss13 <100052310+Borgessss13@users.noreply.github.com>
2026-05-07 23:03:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const baseDeDados = {
         { id: "slb", nome: "Benfica", foto: "fotos/benfica.png" }
     ],
     "portugal": [
-        { id: "pt_home", nome: "Portugal Principal 2026", foto: "ptprin2526.jpg" },
+        { id: "pt_home", nome: "Portugal Principal 2026", foto: "ptprin2627.jpg" },
         { id: "pt_away", nome: "Portugal Away 2026", foto: "ptaway2526.jpg" },
         { id: "pt_treino", nome: "Portugal Treino 2026", foto: "pttreino2526.jpg" },
         { id: "pt_retro", nome: "Portugal Edição Retro", foto: "ptretro2526.jpg" }
